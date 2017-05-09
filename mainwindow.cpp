378970305c2c239334cdbb01e25cef3e674271e9
midnightgalaxy <joakim.larrsson.hem@gmail.com>
2017-05-09 16:39:34 +0200

    gui more or less finished

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,43 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowTitle("Calculator");
+    operStr = "";
+    ui->lineEdit->setText("");
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::slot2(QString x)
+{
+    operStr = x.toStdString();
+}
+
+void MainWindow::setLineEdit(const char * str)
+{
+    operStr += str;
+    ui->lineEdit->setText(operStr.c_str());
+}
+
+void MainWindow::resetLineEdit()
+{
+    operStr = "";
+    ui->lineEdit->setText("");
+}
+
+void MainWindow::slotC()
+{
+    operStr = "";
+    ui->lineEdit->setText("");
+}
+
+void MainWindow::slotCopy()
+{
+    QApplication::clipboard()->setText(operStr.c_str());
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QApplication>
+#include <QClipboard>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +17,41 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    void slot1() { QApplication::aboutQt(); }
+    void slot2(QString);        // setting operStr from inputfield
+
+    void slotC();
+    void slotEnter() {}
+
+    void slotNum1() { setLineEdit("1"); }
+    void slotNum2() { setLineEdit("2"); }
+    void slotNum3() { setLineEdit("3"); }
+    void slotNum4() { setLineEdit("4"); }
+    void slotNum5() { setLineEdit("5"); }
+    void slotNum6() { setLineEdit("6"); }
+    void slotNum7() { setLineEdit("7"); }
+    void slotNum8() { setLineEdit("8"); }
+    void slotNum9() { setLineEdit("9"); }
+    void slotNum0() { setLineEdit("0"); }
+
+    void slotDot() { setLineEdit("."); }
+
+    void slotPlus() { setLineEdit("+"); }
+    void slotSub() { setLineEdit("-"); }
+    void slotMult() { setLineEdit("*"); }
+    void slotDiv() { setLineEdit("/"); }
+
+    void slotCopy();
+
+
+
 private:
     Ui::MainWindow *ui;
+    std::string operStr;
+
+    void setLineEdit(const char * str);
+    void resetLineEdit();
 };
 
 #endif // MAINWINDOW_H
